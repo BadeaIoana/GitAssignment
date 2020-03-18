fe80545796adf4d208ceaa8f6d11c1e7afe3a697
BadeaIoana <62351923+BadeaIoana@users.noreply.github.com>
2020-03-18 20:27:18 +0200

    commit initial

---
+++ CTSTemaGit/src/ro/ase/csie/cts/gr1074/TestGit.java
@@ -0,0 +1,11 @@
+package ro.ase.csie.cts.gr1074;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Git ! Denumirea proiectului de licenta este 'Aplicatie mobila pentru schimb de carti'.");
+
+	}
+
+}
